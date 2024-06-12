9c35f49b61147beaa4637d2b2a900116d7c45a2f
dave-InTheCloud <davidlambiase@gmail.com>
2024-06-13 01:22:57 +0200

    refactor localstorage

--- app/services/LocalStorageService.tsx
+++ app/services/LocalStorageService.tsx
@@ -9,54 +9,32 @@ export interface IStorageService<T> {
 
 export class StorageService<T> implements IStorageService<T> {
   private readonly prefix: string;
-  private readonly isWeb: boolean;
   private readonly sync: boolean;
 
   constructor(prefix: string = '', sync: boolean = false) {
     this.prefix = prefix;
-    this.isWeb = Platform.OS === 'web';
     this.sync = sync;
   }
 
   async get(key: string): Promise<T | null> {
-    if (this.isWeb) {
-      const item = localStorage.getItem(this.prefix + key);
-      if (!item) return null;
-      return JSON.parse(item) as T;
-    } else {
-      if (this.sync) {
-        const item = AsyncStorage.getItem(this.prefix + key);
-        if (!item) return null;
-        return JSON.parse(item) as T;
-      } else {
-        const item = await AsyncStorage.getItem(this.prefix + key);
-        if (!item) return null;
-        return JSON.parse(item) as T;
-      }
-    }
+    const item = await AsyncStorage.getItem(this.prefix + key);
+    if (!item) return null;
+    return JSON.parse(item) as T;
   }
 
   async set(key: string, value: T): Promise<void> {
-    if (this.isWeb) {
-      localStorage.setItem(this.prefix + key, JSON.stringify(value));
+    if (!this.sync) {
+      AsyncStorage.setItem(this.prefix + key, JSON.stringify(value));
     } else {
-      if (this.sync) {
-        AsyncStorage.setItem(this.prefix + key, JSON.stringify(value));
-      } else {
-        await AsyncStorage.setItem(this.prefix + key, JSON.stringify(value));
-      }
+      await AsyncStorage.setItem(this.prefix + key, JSON.stringify(value));
     }
   }
 
   async remove(key: string): Promise<void> {
-    if (this.isWeb) {
-      localStorage.removeItem(this.prefix + key);
+    if (!this.sync) {
+      AsyncStorage.removeItem(this.prefix + key);
     } else {
-      if (this.sync) {
-        AsyncStorage.removeItem(this.prefix + key);
-      } else {
-        await AsyncStorage.removeItem(this.prefix + key);
-      }
+      await AsyncStorage.removeItem(this.prefix + key);
     }
   }
 }
